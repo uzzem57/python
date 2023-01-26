5a2ee8aa1de7f59a7cea9ac1415f95d59bdef5d8
Denis Dunaev <uzzem57@mail.ru>
2023-01-26 22:15:57 +0300

    Added solution to problem 02

--- Seminar1/home_work02.py
+++ Seminar1/home_work02.py
@@ -1,3 +1,17 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+a = int(input())
+summa = 0
+while a > 0:
+    summa += a%10
+    a = a//10
+print(summa)
+
+# Второй вариант
+# a = input()
+# summa = 0
+# for i in range(0, len(a)):
+#    summa = summa + int(a[i])
+# print(summa)
