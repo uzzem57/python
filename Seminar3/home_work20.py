e964d90b7e75ec2ca064a262ea500c24c2439696
Denis Dunaev <uzzem57@mail.ru>
2023-02-02 23:56:40 +0300

    Added solution to problem 20

--- Seminar3/home_work20.py
+++ Seminar3/home_work20.py
@@ -1,9 +1,47 @@
-# Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
-# В случае с английским алфавитом очки распределяются так: ● ● ● ● ● ● ● A, E, I, O, U, L, N, S, T, R – 1 очко;
-# D, G – 2 очка; B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка; K – 5 очков; J, X – 8 очков; Q, Z – 10 очков.
-# А русские буквы оцениваются так: ● ● ● ● ● ● ● А, В, Е, И, Н, О, Р, С, Т – 1 очко; Д, К, Л, М, П, У – 2 очка;
-# Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков.
-# Напишите программу, которая вычисляет стоимость введенного пользователем слова. Будем считать, что на вход
-# подается только одно слово, которое содержит либо только английские, либо только русские буквы.
-# Ввод: ноутбук
-# Вывод: 12
+my_list1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R']
+my_list2 = ['D', 'G']
+my_list3 = ['B', 'C', 'M', 'P']
+my_list4 = ['F', 'H', 'V', 'W', 'Y' ]
+my_list5 = ['K']
+my_list6 = ['J', 'X']
+my_list7 = ['Q', 'Z']
+my_list8 = ['А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
+my_list9 = ['Д', 'К', 'Л', 'М', 'П', 'У']
+my_list10 = ['Б', 'Г', 'Ё', 'Ь', 'Я']
+my_list11 = ['Й', 'Ы']
+my_list12 = ['Ж', 'З', 'Х', 'Ц', 'Ч']
+my_list13 = ['Ш', 'Э', 'Ю']
+my_list14 = ['Ф', 'Щ', 'Ъ']
+print("Введите слово: ")
+n = input().upper()
+my_count = 0
+for i in range(len(n)):
+    if my_list1.count(n[i]) > 0:
+        my_count += 1
+    elif my_list2.count(n[i]) > 0:
+        my_count += 2
+    elif my_list3.count(n[i]) > 0:
+        my_count += 3
+    elif my_list4.count(n[i]) > 0:
+        my_count += 4
+    elif my_list5.count(n[i]) > 0:
+        my_count += 5
+    elif my_list6.count(n[i]) > 0:
+        my_count += 8
+    elif my_list7.count(n[i]) > 0:
+        my_count += 10
+    elif my_list8.count(n[i]) > 0:
+        my_count += 1
+    elif my_list9.count(n[i]) > 0:
+        my_count += 2
+    elif my_list10.count(n[i]) > 0:
+        my_count += 3
+    elif my_list11.count(n[i]) > 0:
+        my_count += 4
+    elif my_list12.count(n[i]) > 0:
+        my_count += 5
+    elif my_list13.count(n[i]) > 0:
+        my_count += 8
+    elif my_list14.count(n[i]) > 0:
+        my_count += 10
+print(my_count)
