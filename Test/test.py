43979f64844b8f45d50359d81c75ec5602d42794
Denis Dunaev <uzzem57@mail.ru>
2023-02-06 23:04:16 +0300

    Added solution to problem 22

--- Test/test.py
+++ Test/test.py
@@ -1,14 +1,8 @@
-n = 4
-m = 5
-triangle = []
-for i in range(1):
-    triangle.append([1]*(m))
-for i in range(1, n):
-    triangle.append([1] + [0]*m)
-for i in range(1, n):
-    for j in range(1, m):
-        triangle[i][j] = triangle[i][j-1] + triangle[i-1][j]
-for i in range(0, n):
-    for j in range(0, m):
-        print(triangle[i][j], end=" ")
-    print()
+
+num = [1, 2, 3, 5, 1, 5, 3, 10]
+print([i for i in num if num.count(i) == 1])
+
+
+
+
+
