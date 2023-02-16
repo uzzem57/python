470822517ab82e34ee81c1c8bad9c31c7bfdaa88
Denis Dunaev <uzzem57@mail.ru>
2023-02-16 23:00:21 +0300

    Added solution to problem 36

--- Seminar7/home_work36.py
+++ Seminar7/home_work36.py
@@ -3,9 +3,12 @@
 # Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
 # Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). Примечание: бинарной операцией
 # называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
-# Ввод:      Вывод: print_operation_table(lambda x, y: x * y)
-# 1         2         3         4           5        6
-# 2         4         6         8           10       12
-# 3         6         9        12           15       18
-# 4         8         12       16           20       24
-# 5         10        15       20           25      30                                                                                                     6        12        18       24       30      36
+
+def print_operation_table(operation, num_rows, num_columns):
+    for i in range(1, num_rows + 1):
+        my_list = []
+        for j in range(1, num_columns + 1):
+            my_list.append(str(operation(i, j)))
+        print(''.join(f'{e:<4}' for e in my_list))
+
+print_operation_table(lambda x, y: x * y, 6, 6)
